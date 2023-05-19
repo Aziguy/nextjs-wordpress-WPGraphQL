3cf7751a2f3608b51aea126b8c6abd871c7ba96d
Aziguy <monsieur_hernandez@hotmail.com>
2023-05-19 15:48:53 +0200

    Heading, Columns & Gallery components.

---
+++ components/Columns/Columns.js
@@ -0,0 +1,23 @@
+export const Columns = ({
+    isStackedOnMobile,
+    children,
+    textColor,
+    backgroundColor,
+  }) => {
+    const textColorStyle = textColor ? { color: textColor } : {};
+    const backgroundColorStyle = backgroundColor ? { backgroundColor } : {};
+    return (
+      <div
+        className="my-10"
+        style={{ ...textColorStyle, ...backgroundColorStyle }}
+      >
+        <div
+          className={`max-w-5xl mx-auto ${
+            isStackedOnMobile ? "block md:flex" : "flex"
+          }`}
+        >
+          {children}
+        </div>
+      </div>
+    );
+  };
